badc4a3b9891391558470a0e6336ee33b789733d
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-17 15:39:29 +0530

    Create FindAreaOfTriangle.js

---
+++ FindAreaOfTriangle.js
@@ -0,0 +1,4 @@
+var b = prompt("Enter the base of the triangle: ");    
+var h = prompt("Enter the height of the triangle: ");
+const area = 1/2 * b * h;        
+console.log("area of Trinagle: " + area);
