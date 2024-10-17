86b917b682279c1ca72577ac2eae814f8cf9e8f9
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-17 15:34:13 +0530

    Armstrong.js

---
+++ Armstrong.js
@@ -0,0 +1,23 @@
+function Armstrong(num)
+{
+            let originalNum = num;
+            let r = 0;
+  
+            while(num>0)
+            {
+                let l = num % 10;
+                r = r+(l*l*l);
+                num = parseInt(num/10);
+            }
+  
+            if(originalNum==r)
+            {
+                document.write(originalNum + " is an Armstrong Number")
+            }
+            else{
+                document.write(originalNum + " is not an Armstrong Number")
+            }
+}
+        
+        var num = parseInt(prompt("Enter a number: "))
+        Armstrong(num);
