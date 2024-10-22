5532c40f054015ec18e9d70b7f5065831da2f34b
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-22 23:44:28 +0530

    Create CelsiustoFahrenheit.js

---
+++ CelsiustoFahrenheit.js
@@ -0,0 +1,4 @@
+var cel = parseInt(prompt("Enter temperature in Celsius: "));
+const f = (cel * 9/5 ) + 32;
+document.write(cel + "°C is equal to " + f + "°F");
+console.log(cel + "°C is equal to " + f + "°F");
