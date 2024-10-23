81cefd76930f5c22500b1bdc3bef6fab5faae245
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-23 23:03:11 +0530

    Create CheckPositiveOrNegativeNumber.js

---
+++ CheckPositiveOrNegativeNumber.js
@@ -0,0 +1,19 @@
+let num = parseInt(prompt("Enter a number: "));
+
+if(num>0)
+{
+      document.write(num + " is a positive number.");
+        
+}
+else if(num<0)
+{
+            
+      document.write(num + " is negative number.");
+        
+}
+else
+{
+            
+      document.write(num + " is zero.")
+        
+}
