78ed11aea61a7244e6e5699f7256ec4ecbb70cd2
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-25 21:31:41 +0530

    Create SumOfNaturalNum.js

---
+++ SumOfNaturalNum.js
@@ -0,0 +1,12 @@
+function sum(x)
+            {
+                let sum = 0;
+                for(let i=0; i<=x; i++)
+                {
+                    sum += i;
+                }
+                //console.log("Sum of first natural numbers : " + sum)
+                return sum;
+}
+console.log("Sum of first natural numbers : " + sum(10));
+    
