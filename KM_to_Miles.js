8e360a400c43d8bf24232e795a50ee7c2f59372b
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-27 00:00:18 +0530

    Create KM_to_Miles.js

---
+++ KM_to_Miles.js
@@ -0,0 +1,7 @@
+function convertKilometersToMiles()
+            {
+                var km = prompt("Enter the distance in kilometers: ");
+                const Miles = km * 0.621371;
+                alert(km + " km is equals to : " + Miles + "miles.")
+            }
+            convertKilometersToMiles();
