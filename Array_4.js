3f980469d60422697e0d40ea41f7d008a9818210
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-28 22:46:42 +0530

    Create Array_4.js

---
+++ Array_4.js
@@ -0,0 +1,11 @@
+function display(arr)
+{
+                for(let i=arr.length-1; i>=0; i--)
+                {
+                    console.log(arr[i]);
+                }
+
+}
+
+let Employee = [101,"varsha","DevOps",90000,1];
+display(Employee);
