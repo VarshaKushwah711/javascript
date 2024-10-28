7d097aa1f694fc10cd41a817c5216e77ae3910e5
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-28 22:40:44 +0530

    Create Array_1.js

---
+++ Array_1.js
@@ -0,0 +1,19 @@
+//Declare array And Print 
+let studentarr = [101,"varsha","indore","21"];
+console.log(studentarr);
+
+//size of array
+console.log("size : " + studentarr.length);
+
+//for loop
+for(let i=0; i<studentarr.length; i++)
+{
+     console.log(studentarr[i]);
+}
+            
+//for-of loop not using index value
+            
+for(let i of studentarr)
+{
+      console.log(i);
+}
