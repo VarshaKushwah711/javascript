f3c43d88eff7b6bd115351fb92c3607851ac42e8
Varsha Kushwah <varshakushwah711@gmail.com>
2024-10-28 22:44:59 +0530

    Create Array_3.js

---
+++ Array_3.js
@@ -0,0 +1,11 @@
+//reverse array...
+            
+            let arr = [3,9,54,65,45,67,8];
+            //console.log("size : " + arr.length);
+            console.log("Original array : " + arr);
+
+            console.log("Reverse array : ")
+            for(let i=arr.length-1; i>=0; i--)
+            {
+                console.log(arr[i]);
+            }
