70674ef40bd4976c6e643548f58fc0ff487bdec4
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-13 21:22:45 +0530

    Create convertLowercaseStringToUppercase1.js

---
+++ convertLowercaseStringToUppercase1.js
@@ -0,0 +1,5 @@
+//*4. Write a program to convert lowercase string to uppercase.
+            let str = prompt("Enter a string : ");
+            
+            console.log("Original string : " + str);
+            console.log("In lowercase : " + str.toLowerCase());
