ff577adfb8f1c5e86ff04f992f1ca61c6b200792
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-14 23:44:17 +0530

    Create Array_bubbleShort.js

---
+++ Array_bubbleShort.js
@@ -0,0 +1,17 @@
+let arr = [32,54,23,67,87,45,9,5];
+            for(let i=0; i<arr.length; i++)
+            {
+                for(let j=i+1; j<arr.length; j++)
+                {
+
+                    let temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+            for(let i of arr)
+            {
+                console.log
+            }
+      
