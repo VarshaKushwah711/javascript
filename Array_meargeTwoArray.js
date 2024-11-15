3a6bd1bdddee9b5e11e64e4e8dd84b42343ef48a
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-15 18:22:03 +0530

    Create Array_meargeTwoArray.js

---
+++ Array_meargeTwoArray.js
@@ -0,0 +1,7 @@
+            let a = [1,2,3,4];
+            let b = [5,6,7,8];
+            let c = [...a , ...b];
+            for(let i of c)
+            {
+                console.log(i);
+            }
