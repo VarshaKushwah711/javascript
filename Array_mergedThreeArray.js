9656d26674c644c4270a1ce805ea5870a454cb2c
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-16 18:05:08 +0530

    Create Array_mergedThreeArray.js

---
+++ Array_mergedThreeArray.js
@@ -0,0 +1,8 @@
+let varsha = [87,62,93,87,76];
+            let mansi = [74,85,66,78,67];
+            let shivani = [77,68,59,45,98];
+            let total = [...varsha, ...mansi, ...shivani];
+            for(let i of total)
+            {
+                console.log(i);
+            }
