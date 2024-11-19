f32bbe2cc84873401a26f353e302c963e684aef8
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-19 22:14:54 +0530

    Create function1_Statment.js

---
+++ function1_Statment.js
@@ -0,0 +1,9 @@
+//function declaration 
+
+//console.log(greet("varsha kushwah..")); //can be hoisted
+            
+function greet(name)
+{
+      return "hello " + name + "!";
+}
+console.log(greet("varsha"));
