7b2e38923117622e356293a09ebf271cf0fdf168
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-22 22:31:25 +0530

    String_ConvertLowerCaseToUppercase.js

--- String_ConvertLowerCaseToUppercase.js
+++ String_ConvertLowerCaseToUppercase.js
@@ -1,11 +1,5 @@
-<html>
-    <body>
-        <script>
-            //*4. Write a program to convert lowercase string to uppercase.
+//*4. Write a program to convert lowercase string to uppercase.
             let str = prompt("Enter a string : ");
             
             console.log("Original string : " + str);
             console.log("In lowercase : " + str.toLowerCase());
-        </script>
-    </body>
-</html>
