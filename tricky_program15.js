0e1cd65ad7f48e1cb1558001a72761b560054b07
Varsha Kushwah <varshakushwah711@gmail.com>
2024-12-08 23:45:01 +0530

    Add files via upload

---
+++ tricky_program15.js
@@ -0,0 +1,5 @@
+console.log(0.1 + 0.2 === 0.3);
+
+// false
+
+// Explanation: Due to floating-point precision issues in JavaScript (and most other languages), 0.1 + 0.2 doesn't result in exactly 0.3. It results in something like 0.30000000000000004, which is not strictly equal to 0.3.
