ed719c24f60e2a5adb7e6a8dbeab854d0e52466c
Varsha Kushwah <varshakushwah711@gmail.com>
2024-12-28 23:58:48 +0530

    Create arrayFirstRepeatingElement.js

---
+++ arrayFirstRepeatingElement.js
@@ -0,0 +1,24 @@
+//Q.2 Find the first repeating element in array of integers.
+        let arr = [12,23,354,54,75,354,23,1,2];
+        let isRepeating = false;
+
+        for(let i=0; i<arr.length; i++)
+        {
+
+            for(let j=i+1; j<arr.length; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    console.log("First repeating element in array: " + arr[i]);
+                    isRepeating = true;
+                    break;
+                }
+            
+            }
+
+            if(isRepeating)
+                break;
+        
+        }
+
+        !isRepeating && console.log("No repeating element in array.....");
